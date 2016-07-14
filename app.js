4450dfaa0a7cc2101d99ef2f23c3190a22211dda
jarvan-git <syfiten94416@gmail.com>
2016-07-14 12:19:33 +0800

    added ngMessages

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 (function(){
-	/**
-     * You must include the dependency on 'ngMaterial' 
-     */
-    angular.module('BlankApp', ['ngMaterial']);
+	angular.module('ptso', ['ngMessages']);
+	var app = angular.module('ptso', ['ngMaterial']);
+app.controller('TitleController', function($scope) {
+  $scope.title = 'PTSO';
+});
+app.controller('AppCtrl', function($scope) {
+  $scope.todos = [];
+  for (var i = 0; i < 15; i++) {
+    $scope.todos.push({
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands."
+    });
+  }
+});
 })();
