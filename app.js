bd57988f6903e128446913f9f7076b5b77744889
jarvan-git <syfiten94416@gmail.com>
2016-07-14 12:39:47 +0800

    add card example and load icon from google

--- app.js
+++ app.js
@@ -4,14 +4,5 @@
 app.controller('TitleController', function($scope) {
   $scope.title = 'PTSO';
 });
-app.controller('AppCtrl', function($scope) {
-  $scope.todos = [];
-  for (var i = 0; i < 15; i++) {
-    $scope.todos.push({
-      what: "Brunch this weekend?",
-      who: "Min Li Chan",
-      notes: "I'll be in your neighborhood doing errands."
-    });
-  }
-});
+
 })();
